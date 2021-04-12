1fdb563121f591db74d9c85325fdbe94ced69612
jiawei22 <jiawei22@staff.sina.com.cn>
2021-04-12 22:50:28 +0800

    第二天增添context和路由

--- gee/gee.go
+++ gee/gee.go
@@ -1,23 +1,89 @@
 package gee
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
-type HandlerFunc func(w http.ResponseWriter, r *http.Request)
+type H map[string]interface{}
+
+type Context struct {
+	Writer http.ResponseWriter
+	Req *http.Request
+	Path string
+	Method string
+	StatusCode int
+}
+
+func NewContext(w http.ResponseWriter, r *http.Request) *Context{
+	return &Context{
+		Writer: w,
+		Req: r,
+		Path: r.URL.Path,
+		Method: r.Method,
+	}
+}
+
+func (c *Context) PostFrom(key string) string {
+	return c.Req.FormValue(key)
+}
+
+func (c *Context) Query (key string) string {
+	return c.Req.URL.Query().Get(key)
+}
+
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	c.Writer.WriteHeader(code)
+}
+
+func (c *Context) SetHeader(key string, value string) {
+	c.Writer.Header().Set(key, value)
+}
+
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.SetHeader("Content-Type", "text/plain")
+	c.Status(code)
+	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+}
+
+func (c *Context) JSON(code int, obj interface{})  {
+	c.SetHeader("Content-Type", "application/json")
+	c.Status(code)
+	encoder := json.NewEncoder(c.Writer)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(c.Writer, err.Error(), 500)
+	}
+}
+
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	c.Writer.Write(data)
+}
+
+func (c *Context) HTML(code int, html string) {
+	c.SetHeader("Content-Type", "text/html")
+	c.Status(code)
+	c.Writer.Write([]byte(html))
+}
+
+
+//type HandlerFunc func(w http.ResponseWriter, r *http.Request)
+type HandlerFunc func(ctx *Context)
 
 type Engine struct {
-	router map[string]HandlerFunc
+	router *router
 }
 
 func New() *Engine {
-	return &Engine{router: make(map[string]HandlerFunc)}
+	return &Engine{router: newRouter()}
 }
 
 func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc)  {
-	key := method + "-" + pattern
-	engine.router[key] = handler
+	/*key := method + "-" + pattern
+	engine.router[key] = handler*/
+	engine.router.addRoute(method, pattern, handler)
 }
 
 func (engine *Engine) Get(pattern string, handler HandlerFunc) {
@@ -33,10 +99,12 @@ func (engine *Engine) Run(addr string) (err error) {
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request)  {
-	key := req.Method + "-" + req.URL.Path
+	/*key := req.Method + "-" + req.URL.Path
 	if handler, ok := engine.router[key]; ok {
 		handler(w, req)
 	} else {
 		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
-	}
+	}*/
+	c := NewContext(w, req)
+	engine.router.handle(c)
 }
--- main.go
+++ main.go
@@ -9,7 +9,6 @@ package main
 // 404 NOT FOUND: /world
 
 import (
-	"fmt"
 	"net/http"
 
 	"gee"
@@ -17,13 +16,28 @@ import (
 
 func main() {
 	r := gee.New()
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	//day1
+	/*r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	})*/
+	r.Get("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>hello Gee</h1>")
 	})
-	r.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
+	//day2
+	/*r.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
 		for k,v := range r.Header {
 			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
 		}
+	})*/
+	r.Get("/hello", func(c *gee.Context) {
+		c.String(http.StatusOK, "hello %s, youre at %s \n", c.Query("name"), c.Path)
+	})
+
+	r.Post("/login", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{
+			"username": c.PostFrom("username"),
+			"password": c.PostFrom("password"),
+		})
 	})
 
 	r.Run(":9999")
